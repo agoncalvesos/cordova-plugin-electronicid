5cee1c62eb030cc6f60d9ca88481eb099f4023e4
André Gonçalves <andre.goncalves@outsystems.com>
2020-05-19 13:07:23 +0100

    Added Alamofire, AlamofireImage, ObjectMapper, SocketRocket and VideoId as frameworks instead of cocoapods

---
+++ src/ios/libs/VideoID.framework/Headers/VideoID.h
@@ -0,0 +1,19 @@
+//
+//  VideoID.h
+//  VideoID
+//
+//  Created by arturo on 10/04/2018.
+//  Copyright © 2018 Electronic ID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoID.
+FOUNDATION_EXPORT double VideoIDVersionNumber;
+
+//! Project version string for VideoID.
+FOUNDATION_EXPORT const unsigned char VideoIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoID/PublicHeader.h>
+
+
